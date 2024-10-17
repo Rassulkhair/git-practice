f2f2eb917a9b56970fcf278281eb0c4c544e26ce
Rassulkhair <rassulkhairshokanov@gmail.com>
2024-10-17 19:03:10 +0500

    Changed main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 10;
+        int b = 15;
+        System.out.println(a + b);
     }
 }
